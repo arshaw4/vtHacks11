4345c87220a69c7030f5e36a4552c443fb40d2d0
arshaw4 <alex04shaw@gmail.com>
2023-09-16 22:44:53 -0400

    JS

--- src/script.js
+++ src/script.js
@@ -1,16 +1,29 @@
+const dueDateVar = new Date(2023,8,17,8);
+pastDue = false;
+document.getElementById("currentDate").innerHTML = "The current time and date is " + new Date();
+document.getElementById("dueDate").innerHTML = "VT Hacks 11 is due " + dueDateVar;
 document.getElementById('dueBtn').onclick = function(){
-    document.getElementById('due').innerHTML = dueDate();
+    dueResultText = document.getElementById('due');
+    dueResultText.innerHTML = dueDate();
+    dueResultText.style.color=dueColorChange();
+    dueResultText.style.fontWeight = "bold";
 };
 
 function dueDate(){
     date = new Date();
-    year = date.getFullYear();
-    month = date.getMonth()+1;
-    day = date.getDate();
-    dueDateVar = new Date(2023,8,17,8);
     if(date<dueDateVar){
+        pastDue = false;
         return "Before due date";
     } else{
+        pastDue = true;
         return "Past due date";
     }
 };
+
+function dueColorChange(){
+    if(pastDue){
+        return "red";
+    } else{
+        return "green";
+    }
+}
